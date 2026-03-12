85e730210c71cd412cbe0553a62a9888e31d3ec9
heshamalmosawi <hishamalmosawii@gmail.com>
2026-03-12 15:59:33 +0300

    feat(apigw): setup the gateway with discovery locator. connected to eureka, cors configured

--- backend/apigateway-service/src/main/java/com/sayedhesham/travelorch/apigateway_service/ApigatewayServiceApplication.java
+++ backend/apigateway-service/src/main/java/com/sayedhesham/travelorch/apigateway_service/ApigatewayServiceApplication.java
@@ -2,12 +2,22 @@ package com.sayedhesham.travelorch.apigateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class ApigatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayServiceApplication.class, args);
+	}
+
+	@GetMapping("/ping")
+	public String ping(){
+		return String.format("200 OK");
 	}
 
 }
