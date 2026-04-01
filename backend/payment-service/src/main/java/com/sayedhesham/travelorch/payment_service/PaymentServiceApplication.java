f785c094864735718d1c6cbb5ba54144a3289ccf
heshamalmosawi <hishamalmosawii@gmail.com>
2026-04-01 23:52:02 +0300

    refactor: simplify EntityScan and EnableJpaRepositories annotations in service applications

--- backend/payment-service/src/main/java/com/sayedhesham/travelorch/payment_service/PaymentServiceApplication.java
+++ backend/payment-service/src/main/java/com/sayedhesham/travelorch/payment_service/PaymentServiceApplication.java
@@ -6,22 +6,8 @@ import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {
-	"com.sayedhesham.travelorch.common.entity.user",
-	"com.sayedhesham.travelorch.common.entity.travel",
-	"com.sayedhesham.travelorch.common.entity.accommodation",
-	"com.sayedhesham.travelorch.common.entity.activity",
-	"com.sayedhesham.travelorch.common.entity.transportation",
-	"com.sayedhesham.travelorch.common.entity.payment"
-})
-@EnableJpaRepositories(basePackages = {
-	"com.sayedhesham.travelorch.common.repository.user",
-	"com.sayedhesham.travelorch.common.repository.travel",
-	"com.sayedhesham.travelorch.common.repository.accommodation",
-	"com.sayedhesham.travelorch.common.repository.activity",
-	"com.sayedhesham.travelorch.common.repository.transportation",
-	"com.sayedhesham.travelorch.common.repository.payment"
-})
+@EntityScan("com.sayedhesham.travelorch.common.entity")
+@EnableJpaRepositories("com.sayedhesham.travelorch.common.repository")
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
