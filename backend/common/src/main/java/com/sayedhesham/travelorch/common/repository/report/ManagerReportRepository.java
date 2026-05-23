f8a321200582443432ac293cb76566cae44f5b44
heshamalmosawi <hishamalmosawii@gmail.com>
2026-05-23 23:35:59 +0300

    feat: add ManagerReport entity and repository for managing reports

---
+++ backend/common/src/main/java/com/sayedhesham/travelorch/common/repository/report/ManagerReportRepository.java
@@ -0,0 +1,13 @@
+package com.sayedhesham.travelorch.common.repository.report;
+
+import com.sayedhesham.travelorch.common.entity.report.ManagerReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ManagerReportRepository extends JpaRepository<ManagerReport, Long> {
+
+    long countByManagerId(Long managerId);
+
+    boolean existsByManagerIdAndReporterId(Long managerId, Long reporterId);
+}
